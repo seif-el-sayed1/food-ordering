3321fe31a58c725e72616add84be392e245a4587
Seif <seifsayed121@gmail.com>
2025-03-13 03:42:39 +0200

    Navbar, Sidebar Admin Panel

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -4,7 +4,7 @@ import list from "../assets/order_icon.png"
 
 export const Sidebar = () => {
     return (
-        <div className='w-40 border-r-1 border-gray-500 h-dvh ml-10 pt-10'>
+        <div className='w-55 border-r-1 border-gray-500 h-dvh ml-10 pt-10'>
             <div className='flex gap-5 my-3 p-2 w-full border-1 border-gray-500 cursor-pointer'>
                 <img className='w-5' src={add} alt="ADD" />
                 <p>Add Items</p>
