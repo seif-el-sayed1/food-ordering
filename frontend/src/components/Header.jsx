03ab71639a0400e730ff598bc8b5798cd3e3f6bd
Seif <seifsayed121@gmail.com>
2025-03-17 20:47:35 +0200

    Edit Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import header from "../assets/header.jpg"
-import { useNavigate } from 'react-router-dom'
-
 
 export const Header = () => {
-    const navigate = useNavigate()
     return (
         <div style={{backgroundImage:`url(${header})`, backgroundSize:"cover"}} 
             className="container mx-20 pl-10 w-auto h-100 bg-orange-600 rounded-xl relative">
@@ -14,9 +11,9 @@ export const Header = () => {
                         ingredients and culinary expertise. our mission is to satisfy your cravings and 
                         elevate <br /> your dining experience,  one delicious meal at all time.
                     </p>
-                    <button onClick={() => navigate("/getStarted")} className='rounded-full text-blue-950 text-sm
+                    <button  className='rounded-2xl w-25 text-blue-950 text-sm
                     cursor-pointer px-4 py-3 my-3 bg-white hover:bg-blue-950 
-                    hover:text-white transition'>Get Started</button>
+                    hover:text-white transition'><a href="#menu">Menu</a></button>
                 </div>
         </div>
     )
